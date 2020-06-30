0bd50b144468ef040f4fe16fa40a2acf0013266d
aso1801008 <1801008@s.asojuku.ac.jp>
2020-06-30 05:40:31 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <hello-world title='Hello'
+    message="これは、Vue.jsのサンプルプロジェクトです"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    message:String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div{
+  margin:0px;
+  padding:0px;
+  text-align:left;
+}
+h1{
+  font-size:72pt;
+  font-weight: bold;
+  text-align: right;
+  letter-spacing: -8px;
+  clolor:#f0f0f0;
+  margin:0px;
+}
+p{
+  margin: 0px;
+  color:#666;
+  font-size: 16pt;
+}
+</style>
